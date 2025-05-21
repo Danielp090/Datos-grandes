511ed8b760133c80f4d50f8059d1b5e9c1176cb4
¨Danielp090¨ <daniel.peralta@es.uazuay.edu.ec>
2025-05-21 10:30:07 -0500

    Cambios con grafica

--- App.py
+++ App.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import plotly.express as px
 st.title("Mi Segunda publicacion")
 st.header("Introduccion")
 st.write("Esta es mi primera pagina")
+fig=px.bar(x=["A","B","C"],y=[5,6,8])
+st.plotly_chart(fig)
+

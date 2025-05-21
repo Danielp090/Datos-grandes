5b8de98c953e51a708b5173a58700bf6019fdaba
¨Danielp090¨ <daniel.peralta@es.uazuay.edu.ec>
2025-05-21 10:40:14 -0500

    Dataset

--- App.py
+++ App.py
@@ -1,8 +1,22 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
+df=pd.read_csv("house_sales.csv")
+
 st.title("Mi Segunda publicacion")
 st.header("Introduccion")
 st.write("Esta es mi primera pagina")
 fig=px.bar(x=["A","B","C"],y=[5,6,8])
 st.plotly_chart(fig)
+st.header("Datos")
+fig=px.scatter(df,x="PropertyType",y="SalePrice")
+st.plotly_chart(fig)
+st.header("Grafica 2")
+fig=px.scatter(df,x="SqFtLot",y="SalePrice")
+st.plotly_chart(fig)
+st.header("Grafica 3")
+fig=px.scatter(df,x="YrBuilt",y="SalePrice")
+st.plotly_chart(fig)
+
+
 
